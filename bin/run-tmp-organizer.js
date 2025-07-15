23df465e0e0b07ce86b7386dd4b19f757c3cc79f
David Fischer <fischerdavid1978@gmail.com>
2025-07-15 09:16:30 +0200

    Verbesserungen an templateOrganizer.js
    - Hinzugefügt: Fehlerbehandlung und Konsolenausgaben für Konfigurationsdateien
    - Umstrukturierung der Funktionen zur besseren Lesbarkeit und Wartbarkeit
    - Konsolidierung von Ladefunktionen für Konfigurations- und Sortierungseinstellungen
    - Einführung von Konstanten für Farbcodes und Dateipfade
    - Verbesserung der Fehlerprotokollierung bei fehlenden Dateien

--- bin/run-tmp-organizer.js
+++ bin/run-tmp-organizer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-import { execSync } from "child_process"
-import { fileURLToPath } from "url"
-import { dirname, join } from "path"
+import {execSync} from "child_process"
+import {fileURLToPath} from "url"
+import {dirname, join} from "path"
 
 // Constants
 const SCRIPT_COMMAND = "npm run sort"
